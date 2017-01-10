759f9d8e33b17f72ef43295cfb6d5a86eecfe193
janreinder <jr.z25@live.nl>
2017-01-10 14:31:19 +0100

    till 6.4

---
+++ src/ss/week6/WrongArgumentException.java
@@ -0,0 +1,5 @@
+package ss.week6;
+
+public class WrongArgumentException extends Exception {
+    
+}
--- src/ss/week6/Zipper.java
+++ src/ss/week6/Zipper.java
@@ -11,20 +11,21 @@ public class Zipper {
             result += Character.toString(s1.charAt(i))
                       + Character.toString(s2.charAt(i));
         }
+        
         return result;
+    }    
+
+    public static void main(String[] args) throws TooFewArgumentsException, ArgumentLengthsDifferException {
+        System.out.println(zip2(args[0], args[1]));
     }
     
-
-    public static void main(String[] args) {
-        String s1 = args.length >= 1 ? args[0] : null;
-        String s2 = args.length >= 2 ? args[1] : null;
+    public static String zip2(String s1, String s2) throws TooFewArgumentsException, ArgumentLengthsDifferException {
         if (s1 == null || s2 == null) {
-            System.out.println("error: must pass two command line arguments");
+            throw new TooFewArgumentsException();            
         } else if (s1.length() != s2.length()) {
-            System.out.println("error: length of command line arguments "
-                    + "differs (" + s1.length() + ", " + s2.length() + ")");
+            throw new ArgumentLengthsDifferException(s1.length(), s2.length());
         } else {
-            System.out.println(zip(s1, s2));
+            return zip(s1, s2);
         }
     }
 }
