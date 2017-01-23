b3dca051d31c73b19ebc663c045973be3e4b91ef
Maximilian Stein <maximilian.stein@astatine.utwente.nl>
2017-01-23 15:28:25 +0100

    basic board functions

--- src/ss/project/Board.java
+++ src/ss/project/Board.java
@@ -1,5 +1,17 @@
 package ss.project;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Board {
+    public static int MAXFIELD = 3;
+    private Map<Field, Mark> fieldMap = new HashMap<Field, Mark>();
     
+    public Board() {
+        this.reset();
+    }
+    
+    public void reset() {
+        this.fieldMap.clear();
+    }
 }
