58710b21919db372cbbda450ee6574212de1dd25
Maximilian Stein <maximilian.stein@astatine.utwente.nl>
2017-01-24 15:38:11 +0100

    Basic functions in board, Field constructor

--- src/ss/project/Board.java
+++ src/ss/project/Board.java
@@ -10,11 +10,45 @@ public class Board {
     public Board() {
         this.reset();
     }
-    //@ensures (field.x > MAXFIELD
+    //@ensures (field.x <= MAXFIELD
     //@pure;
     public boolean isField(Field field) {
-        return field.x < MAXFIELD || field.y < MAXFIELD || field.z < MAXFIELD;
+        return field.x <= MAXFIELD || field.y <= MAXFIELD || field.z <= MAXFIELD;
     }
+    
+    public boolean isEmpty(Field field) {
+        return !this.fieldMap.containsKey(field);
+    }
+    
+    public boolean setField(Field field, Mark m) {
+        if (this.isField(field) && this.isEmpty(field)) {
+            this.fieldMap.put(field, m);
+            return true;
+        }
+        return false;
+    }
+    
+    public Mark getMark(Field field) {
+        return this.fieldMap.get(field);
+    }
+    
+    public Field getFieldByCoordinate(int x,int y, int z) {
+        Field result = new Field(x,y,z);
+        if (this.isField(result)) {
+            return result;
+        }
+        return null;
+    }
+    
+    public int getNextZ(int x, int y) {
+        Field tester = new Field(x,y,0);
+        for (tester.z = 0; tester.z <= MAXFIELD; tester.z++) {
+            if (this.isEmpty(tester)) {
+                return tester.z;
+            }
+        }
+    }
+    
     
     public void reset() {
         this.fieldMap.clear();
--- src/ss/project/Field.java
+++ src/ss/project/Field.java
@@ -4,6 +4,12 @@ public class Field {
     int x,y,z;
     //@invariant x,y,z >= 0;
     
+    public Field(int newx, int newy, int newz) {
+        this.x = newx;
+        this.y = newy;
+        this.z = newz;
+    }
+    
     @Override
     public boolean equals(Object o) {
         if (o instanceof Field) {
