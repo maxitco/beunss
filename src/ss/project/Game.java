ed8829e0f01f08a507fe470d46987b565f46a6b1
Jan Reinder <Jan Reinder@192.168.1.8>
2017-01-27 13:27:57 +0100

    makemove

--- src/ss/project/Game.java
+++ src/ss/project/Game.java
@@ -7,6 +7,7 @@ public class Game extends Thread {
     private Board board;
     private boolean running = false;
     private boolean answered; //used for keeping track of client response
+    private int turnCounter = 1;
     
     public Game() {
         this.board = new Board();
@@ -34,20 +35,49 @@ public class Game extends Thread {
         }
     }
     
-    public synchronized void makeMove() {
+    //make move depending on input, the id needs to be provided directly by the clienthandler
+    public synchronized void makeMove(int x, int y, int id) {
+        //check if it is the turn of the player that calls the function
+        if (playerList.get(turnOfPlayer()).getPlayerId() == id) {
+            
+            //make the move, mark depends on index in the array
+            if (turnOfPlayer() == 0) {
+                //try to make the move, if valid set answered true 
+                if (board.setField(x, y, Mark.Black)) {
+                    answered = true;
+                } else {
+                    //invalid --> return error 5 (invalid move)
+                    playerList.get(turnOfPlayer()).send("error 5");
+                }
+            } else {
+              //try to make the move, if valid set answered true 
+                if (board.setField(x, y, Mark.White)) {
+                    answered = true;
+                } else {
+                    //invalid --> return error 5 (invalid move)
+                    playerList.get(turnOfPlayer()).send("error 5");
+                }                
+            }
+
+        } else {
+            
+        }
         
     }
     
+    //determine whose turn it is, depending on turn and amount of players
+    public int turnOfPlayer() {
+        return this.turnCounter % (this.playerList.size());
+    }
+    
     public void run() {
-        //counts at which turn the game is, start with turn 1
-        int turnCounter = 1;
-        
+                        
         while (true) {
-            //determine whose turn it is, depending on turn and amount of players
-            int i = turnCounter % (playerList.size()-1);
+            
+           
             
             for (ClientHandler c: this.playerList) {
-                c.send(Protocol.Server.TURNOFPLAYER + " " + playerList.get(i).getPlayerId());
+                c.send(Protocol.Server.TURNOFPLAYER + " " + playerList.get(turnOfPlayer()).getPlayerId());
             }
             //set answered on false, is set true when the player whose turn it is makes a move
             this.answered = false;
@@ -56,7 +86,7 @@ public class Game extends Thread {
             while (!answered) {
                 
             }
-            turnCounter++; //next turn
+            this.turnCounter++; //next turn
         }
     }
 }
