34d81eff0ceba92fc64809b10a3107462f802b71
Maximilian Stein <maximilian.stein@astatine.utwente.nl>
2017-01-27 14:20:10 +0100

    Begin boardtest

---
+++ src/ss/project/test/BoardTest.java
@@ -0,0 +1,24 @@
+package ss.project.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class BoardTest {
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
