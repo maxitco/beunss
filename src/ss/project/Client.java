8f654deb89f13d18e5761ff4e58eb72171a416fd
Jan Reinder <Jan Reinder@192.168.1.8>
2017-01-29 16:41:56 +0100

    all commands updated

--- src/ss/project/Client.java
+++ src/ss/project/Client.java
@@ -18,25 +18,44 @@ public class Client extends Terminal {
     private Socket sock;
     private String playerName;
     private int playerId;
-    private Game game;
+    private Board board;
     private ServerHandler serverHandler;
     private boolean online;
     private boolean inGame = false;
+    private int currentTurnId;
     
     
     public Client() throws IOException {
         super(System.in, System.out);
         this.playerName = "NoNamePepe";     //should be overwritten           
     }
+    public void refreshBoard() {
+        this.board = new Board();
+    }
     
     public void setPlayerName(String input) {
-        this.playerName = input;
-        
+        this.playerName = input;        
+    }
+    
+    public void setPlayerId(int input) {
+        this.playerId = input;
+    }
+    
+    public void setCurrentTurnId(int input) {
+        this.currentTurnId = input;
     }
     
     /*@ pure */ public String getPlayerName() {
         return this.playerName;
-    }   
+    }  
+    
+    /*@ pure */ public int getPlayerId() {
+        return this.playerId;
+    }  
+    
+    /*@ pure */ public Board getBoard() {
+        return this.board;
+    }  
     
     /*@ pure */ public boolean isOnline() {
         return this.online;
@@ -52,25 +71,45 @@ public class Client extends Terminal {
     
     @Override
     public void atStart() {
-        send("Enter 'online <server IP adress> <Socket>' to start an online game or 'offline' to play against AI ");        
+        send("Enter 'online <server IP adress> <Socket> <Your player name>' to start an online game or 'offline' to play against AI ");        
     }
     
     @Override
     public void handleInput(String input) {
-      //split input around spaces
-        String[] inputSplit = input.split(" ");
-        
-        if (inputSplit[0].equals("online")) {
-            connectToServer(inputSplit[1], inputSplit[2]);
-            send("connecting to server...");     
-            this.online = true;
-        } else if (inputSplit[0].equals("offline")) {
-            connectToServer("localhost", "2728");
-            send("setting up game...");
-            this.online = false;
-        } else if (inputSplit[0].equals("EXIT")) {
-            System.exit(0);
-        }        
+        if (input != null) {
+            //split input around spaces
+            String[] inputSplit = input.split(" ");
+            
+            if (inputSplit[0].equals("online") && inputSplit.length == 4) {
+                setPlayerName(inputSplit[3]);
+                connectToServer(inputSplit[1], inputSplit[2]);
+                send("connecting to server...");     
+                this.online = true;
+            } else if (inputSplit[0].equals("offline")) {
+                connectToServer("localhost", "2728");
+                send("setting up game...");
+                this.online = false;
+            } else if (inputSplit[0].equals("EXIT")) {
+                System.exit(0);
+            } else if (inputSplit[0].equals("MOVE") && inputSplit.length == 3 && this.inGame) {
+                serverHandler.send(Protocol.Client.MAKEMOVE + " " + inputSplit[1] + " " + inputSplit[2]);
+            } else {
+                send(
+                    "You entered: " + input 
+                    + "which is not valid input or can only be done if it is your turn in the game."
+                );
+                
+                if (this.inGame) {
+                    if (this.currentTurnId != this.playerId) {
+                        send("Note that it is not your turn now");
+                    }
+                } else {
+                    send("Note that you are not in game right now");
+                }          
+            }
+        } else {
+            send("null input, y u do this?");
+        }
     }
     
     public void connectToServer(String ip, String socket) {
