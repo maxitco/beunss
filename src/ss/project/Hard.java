4852727b89f4a1ade1005c43de3c17770b6fe498
Maximilian Stein <maximilian.stein@astatine.utwente.nl>
2017-02-01 10:36:32 +0100

    Hard ai

---
+++ src/ss/project/Hard.java
@@ -0,0 +1,126 @@
+package ss.project;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class Hard implements Difficulty {
+
+	@Override
+	public String getName() {
+		return "hard";
+	}
+
+	@Override
+	public Field determineMove(Board board, Mark m) {
+		
+        ArrayList<Field> moves = this.getMoves(board);
+        Map<Field, Integer> result = this.getResultMap(board, m);
+        
+        //Choose candidate with highest potential score
+        int maxValue = Collections.max(result.values());
+        System.out.println(maxValue);
+        Iterator<Map.Entry<Field, Integer>> it = result.entrySet().iterator();
+        while (it.hasNext()) {
+        	Map.Entry<Field,Integer> item = it.next();
+        	if (item.getValue().intValue() < maxValue) {
+        		it.remove();
+        	}
+        }
+        it = result.entrySet().iterator();
+        int next = (int) (Math.random() * new Double(result.size()));
+        int count = 0;
+        while (it.hasNext()) {
+        	Map.Entry<Field,Integer> item = it.next();
+        	if (count == next) {
+        		System.out.println(m.toString() + " chose " + item.getKey().toString());
+        		return item.getKey();
+        	}
+        	count++;
+        }
+        
+        return null;
+	}
+	//@requires board != null;
+	public ArrayList<Field> getMoves(Board board) {
+		ArrayList<Field> moves = new ArrayList<Field>();
+        for (int x = 0; x <= Board.MAXFIELD; x++) {
+            for (int y = 0; y <= Board.MAXFIELD; y++) {
+                Field empty = board.getEmptyField(x, y);
+                if (empty != null) {
+                    moves.add(empty);
+                }
+            }
+        }
+        return moves;
+	}
+	
+	public int getFutureImpact(Board board, Mark m) {
+		int result = 0;
+		ArrayList<Field> moves = this.getMoves(board);
+		System.out.println(moves.size());
+		for (Field field : moves) {
+			Board myboard = board.copy();
+			Board enemboard = board.copy();
+			myboard.setField(field, m);
+			enemboard.setField(field, m.other());
+			if (myboard.isWinner(m)) {
+				result += 50;
+			}
+			else if (enemboard.isWinner(m.other())) {
+				result -= 20;
+			}
+		}
+		return result;
+	}
+	
+	public Map<Field, Integer> getResultMap(Board board, Mark m) {
+		Map<Field, Integer> result = new HashMap<Field, Integer>();
+		ArrayList<Field> moves = this.getMoves(board);
+		System.out.println(moves.size());
+		for (Field field : moves) {
+			Board myboard = board.copy();
+			Board enemboard = board.copy();
+			myboard.setField(field, m);
+			enemboard.setField(field, m.other());
+			if (myboard.isWinner(m)) {
+				result.put(field, new Integer(500));
+			}
+			else if (enemboard.isWinner(m.other())) {
+				result.put(field, new Integer(100));
+			}
+			else {
+				result.put(field, new Integer(countSurroundings(field, board, m)));
+			}
+			result.put(field, result.get(field) + getFutureImpact(myboard, m));
+		}
+
+		return result;
+	}
+	
+	public int countSurroundings(Field field, Board board, Mark m) {
+    	int result = 0;
+    	for (int z = -1; z <= 0; z++) {
+    		for (int i = -1; i <= 1; i++) {
+    			for (int a = -1; a <= 1; a++) {
+    				Field neighbour = board.walkField(field, i, a, z);
+    				//next to field of same color
+    				if (neighbour != null) {
+    					Mark nextmark = board.getMark(neighbour);
+    					if (nextmark != null && nextmark.equals(m)) {
+        					result++;
+    					}
+    				}
+    				//next to boundary
+    				else if (neighbour == null) {
+    					result++;
+    				}
+    			}
+    		}
+    	}
+    	return result;
+    }
+
+}
--- src/ss/project/test/AITest.java
+++ src/ss/project/test/AITest.java
@@ -6,17 +6,19 @@ public class AITest {
 
 	public static void main(String[] args) {
 		Board test = new Board();
-		ComputerPlayer one = new ComputerPlayer(new Medium());
+		ComputerPlayer one = new ComputerPlayer(new Hard());
 		ComputerPlayer two = new ComputerPlayer(new Medium());
 		Mark m = Mark.Black;
 		while(!test.hasEnded()) {
 			test.setField(one.determineMove(test, m), m);
-			if (test.hasWinner()) {
+			if (test.isWinner(m)) {
+				System.out.println("one has won");
 				System.out.println(test);
 				System.exit(0);
 			}
 			test.setField(two.determineMove(test, m.other()), m.other());
-			if (test.hasWinner()) {
+			if (test.isWinner(m.other())) {
+				System.out.println("two has won");
 				System.out.println(test);
 				System.exit(0);
 			}
